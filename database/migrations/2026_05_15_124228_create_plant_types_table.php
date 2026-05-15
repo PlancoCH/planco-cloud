8004af99b7c24d04fcfc63dbdd90db706b64f203
utigernils <utigernils@gmail.com>
2026-05-15 14:44:18 +0200

    feat; add plant type table

---
+++ database/migrations/2026_05_15_124228_create_plant_types_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('plant_types', function (Blueprint $table) {
+            $table->id();
+            $table->string('common_name');
+            $table->text('description');
+            $table->binary('standard_image');
+            $table->string('scientific_name');
+            $table->float('ideal_temp');
+            $table->float('ideal_moisture');
+            $table->float('ideal_light_lux');
+            $table->float('ideal_humidity');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('plant_types');
+    }
+};
