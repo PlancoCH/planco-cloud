a9dee6e75838917d620fef9864db8aa98e789177
utigernils <utigernils@gmail.com>
2026-05-15 14:53:38 +0200

    feat; create plants table

---
+++ database/migrations/2026_05_15_124659_create_plants_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('plants', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('device_id')->nullable()->constrained()->nullOnDelete();
+            $table->foreignId('plant_type_id')->constrained()->nullOnDelete();
+            $table->binary('custom_image')->nullable();
+            $table->string('nickname');
+            $table->text('notes')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('plants');
+    }
+};
