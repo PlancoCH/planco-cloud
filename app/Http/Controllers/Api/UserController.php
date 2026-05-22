58601d260df8b9585a4e7f85a8d9e25217cabf86
utigernils <utigernils@gmail.com>
2026-05-22 13:30:26 +0200

    feat; implement emial verification

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Auth\Events\Registered;
 use App\Http\Requests\Api\Auth\LoginRequest;
 use App\Http\Requests\Api\Auth\SignupRequest;
 use App\Http\Requests\Api\Auth\UpdateUserRequest;
@@ -21,6 +22,8 @@ class UserController extends Controller
             'email' => $request->validated('email'),
             'password' => $request->validated('password'),
         ]);
+
+        event(new Registered($user));
 
         $token = $user->createToken('api')->plainTextToken;
 
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\UserController;
+use App\Http\Controllers\Api\VerifyEmailController;
 
 Route::prefix('auth')->group(function () {
 	Route::post('/signup', [UserController::class, 'signup']);
@@ -13,6 +14,14 @@ Route::prefix('auth')->group(function () {
 		Route::get('/me', [UserController::class, 'me']);
 		Route::put('/me', [UserController::class, 'update']);
 		Route::patch('/me', [UserController::class, 'update']);
+        Route::post('/email/verification-notification', [VerifyEmailController::class, 'resend'])
+            ->middleware('throttle:6,1')
+            ->name('verification.send');
 	});
 });
+
+Route::get('/email/verify/{id}/{hash}', [VerifyEmailController::class, 'verify'])
+    ->middleware(['signed', 'throttle:6,1'])
+    ->name('verification.verify');
+
 
