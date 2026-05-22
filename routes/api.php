ee9690cb85342123c2e6c79d59afecb249dd9332
utigernils <utigernils@gmail.com>
2026-05-22 14:25:11 +0200

    feat; add endpoints for device controller

--- routes/api.php
+++ routes/api.php
@@ -1,27 +1,33 @@
 <?php
 
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\UserController;
 use App\Http\Controllers\Api\VerifyEmailController;
+use App\Http\Controllers\DeviceController;
+use Illuminate\Support\Facades\Route;
 
 Route::prefix('auth')->group(function () {
-	Route::post('/signup', [UserController::class, 'signup']);
-	Route::post('/login', [UserController::class, 'login']);
+    Route::post('/signup', [UserController::class, 'signup']);
+    Route::post('/login', [UserController::class, 'login']);
 
-	Route::middleware('auth:sanctum')->group(function () {
-		Route::post('/logout', [UserController::class, 'logout']);
-		Route::get('/me', [UserController::class, 'me']);
-		Route::put('/me', [UserController::class, 'update']);
-		Route::patch('/me', [UserController::class, 'update']);
+    Route::middleware('auth:sanctum')->group(function () {
+        Route::post('/logout', [UserController::class, 'logout']);
+        Route::get('/me', [UserController::class, 'me']);
+        Route::put('/me', [UserController::class, 'update']);
+        Route::patch('/me', [UserController::class, 'update']);
         Route::post('/email/verification-notification', [VerifyEmailController::class, 'resend'])
             ->middleware('throttle:6,1')
             ->name('verification.send');
-	});
+    });
 });
 
 Route::get('/email/verify/{id}/{hash}', [VerifyEmailController::class, 'verify'])
     ->middleware(['signed', 'throttle:6,1'])
     ->name('verification.verify');
 
-
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/devices', [DeviceController::class, 'index']);
+    Route::post('/devices/map', [DeviceController::class, 'map']);
+    Route::get('/devices/{device}', [DeviceController::class, 'show']);
+    Route::put('/devices/{device}', [DeviceController::class, 'update']);
+    Route::post('/devices/{device}/unmap', [DeviceController::class, 'unmap']);
+});
