90fc505d6b430c51f4bafec9ca0fe59811782720
utigernils <utigernils@gmail.com>
2026-05-22 14:24:48 +0200

    fix; null on delete when owner of a device is deleted

--- database/migrations/2026_05_15_124428_create_devices_table.php
+++ database/migrations/2026_05_15_124428_create_devices_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('devices', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('user_id')->nullable()->constrained()->nullOnDelete();
             $table->string('name');
             $table->text('notes')->nullable();
             $table->char('api_key', 64)->unique(); // NEEDS TO BE HASHED
